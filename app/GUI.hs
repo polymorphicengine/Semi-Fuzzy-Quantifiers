296d1da352534d239b5c340b81c2fc251b20b3ec
onthepeakofnormal <gius_martin@yahoo.de>
2021-11-17 14:34:15 +0100

    get value of inputs via button click

--- app/GUI.hs
+++ app/GUI.hs
@@ -7,16 +7,27 @@ import Graphics.UI.Threepenny.Core hiding (Const)
 
 setup :: Window -> UI ()
 setup window = void $ do
-    return window # set title "Currency Converter"
+    return window # set title "Semi-Fuzzy-Quantifiers"
 
-    scopeP <- UI.input
-    rangeP  <- UI.input
-    play <- UI.button
+    scopeIn <- UI.input
+    rangeIn  <- UI.input
+    playButton <- UI.button
+                      # set UI.text "Play!"
+    display <- UI.pre
 
     getBody window #+ [
             column [
-                grid [[string "A:", element scopeP]
-                     ,[string "B:"  , element rangeP ]]
-            , element play
-            , string "Amounts update while typing."
+                grid [[string "Scope:", element scopeIn]
+                     ,[string "Range:"  , element rangeIn]]
+            , element playButton
+            , element display
             ]]
+
+    on UI.click playButton $ \_ -> playButtonFunction scopeIn rangeIn display
+
+
+playButtonFunction ::  Element -> Element -> Element -> UI ()
+playButtonFunction scopeIn rangeIn display = do
+                                sc <- scopeIn # get UI.value
+                                rn <- rangeIn # get UI.value
+                                void $ element display # set UI.text sc
