ba3db852641b942f662afae9ce5539e981202e60
tompoise <markus.rinke@gmail.com>
2021-11-23 23:54:02 +0100

    Improved layout; added hovering elements (some fixes needed).

--- app/GUI.hs
+++ app/GUI.hs
@@ -14,6 +14,7 @@ import Game
 {-----------------------------------------------------------------------------
     Main
 ------------------------------------------------------------------------------}
+
 -- main :: IO ()
 -- main = startGUI defaultConfig setup
 
@@ -51,29 +52,85 @@ setup window = void $ do
     Buttons
 ------------------------------------------------------------------------------}
 
+    button <- UI.button # set UI.text " Start game! "
+                        # set style [("background-image","linear-gradient(to bottom right, red,yellow)")]
 
-    button <- UI.button # set UI.text "Start game"
+    on UI.hover button $ \_ -> do
+        element button # set text "Yes, push me!"
 
+    on UI.leave button $ \_ -> do
+        element button # set text " Start game! "
+
+    on UI.click button $ \_ -> do
+        element button # set text " Thonk you! "
 
 {-----------------------------------------------------------------------------
     Input fields
 ------------------------------------------------------------------------------}
 
     field_domain     <- UI.input
-                            # set style [("width","20%")]
+                            # set style [("width","20%"),("text-align","center")]
     field_k          <- UI.input
-                            # set style [("width","20%")]
+                            # set style [("width","20%"),("text-align","center")]
+
     field_m          <- UI.input
-                            # set style [("width","20%")]
+                            # set style [("width","20%"), ("text-align", "center")]
     field_samplesize <- UI.input
-                            # set style [("width","50%")]
+                            # set style [("width","50%"),("text-align","center")]
 
     field_card1      <- UI.input
-                           # set style [("width","20%")]
+                           # set style [("width","20%"),("text-align","center")]
     field_card2      <- UI.input
-                           # set style [("width","20%")]
+                           # set style [("width","20%"),("text-align","center")]
     field_card3      <- UI.input
-                           # set style [("width","20%")]
+                           # set style [("width","20%"),("text-align","center")]
+
+
+ {-----------------------------------------------------------------------------
+     D. Hoverhoff
+ ------------------------------------------------------------------------------}
+
+    text_field_domain       <- UI.span # set text "\\(|D|:\\)"
+    on UI.hover text_field_domain $ \_ -> do
+        element text_field_domain # set text "Give me a domain size!"
+    on UI.leave text_field_domain $ \_ -> do
+        element text_field_domain # set text "\\(|D|:\\)"
+
+    text_field_k            <- UI.span # set text "\\(k:\\)"
+    on UI.hover text_field_k $ \_ -> do
+        element text_field_k # set text "Give me some non-negative integer"
+    on UI.leave text_field_k $ \_ -> do
+        element text_field_k # set text "\\(k:\\)"
+
+    text_field_m            <- UI.span # set text "\\(m:\\)"
+    on UI.hover text_field_m $ \_ -> do
+        element text_field_m # set text "Give me some non-negative integer"
+    on UI.leave text_field_m $ \_ -> do
+        element text_field_m # set text "\\(m:\\)"
+
+    text_field_samplesize   <- UI.span # set text "Sample size:"
+    on UI.hover text_field_samplesize $ \_ -> do
+        element text_field_samplesize # set text "Darf's a bissal mehr sein?"
+    on UI.leave text_field_samplesize $ \_ -> do
+        element text_field_samplesize # set text "Sample size:"
+
+    text_field_card1        <- UI.span # set text "\\( | S \\cap R | :\\)"
+    on UI.hover text_field_card1  $ \_ -> do
+        element text_field_card1  # set text "Size of intersection of scope and range"
+    on UI.leave text_field_card1  $ \_ -> do
+        element text_field_card1  # set text "\\( | S \\cap R | :\\)"
+
+    text_field_card2        <- UI.span # set text "\\( | S \\setminus R | :\\)"
+    on UI.hover text_field_card2 $ \_ -> do
+        element text_field_card2 # set text "Size of scope without range"
+    on UI.leave text_field_card2 $ \_ -> do
+        element text_field_card2 # set text "\\( | S \\setminus R | :\\)"
+
+    text_field_card3        <- UI.span # set text "\\( | R \\setminus S | :\\)"
+    on UI.hover text_field_card3  $ \_ -> do
+        element text_field_card3  # set text "Size of scope without range"
+    on UI.leave text_field_card3  $ \_ -> do
+        element text_field_card3  # set text "\\( | R \\setminus S | :\\)"
 
 {-----------------------------------------------------------------------------
     Layout
@@ -81,38 +138,64 @@ setup window = void $ do
 
     getBody window #+ [element mathjaxScript1,
                        element mathjaxScript2,
-                       UI.p # set text "\\[\\Pi^k_m x . S(x)\\]",
+                       --UI.p # set text "\\[\\Pi^k_m x . S(x)\\]",
+                       UI.p # set text "      Semi-Fuzzy Playground" # set style [("color", "blue"), ("text-align", "center")],
                        row [
                             column [
                                    grid [
-                                           [],
-                                           [element wr_input, element wor_input],
-                                           [string "\\(k:\\)", element field_k],
-                                           [string "\\(m:\\)", element field_m]
-                                        ],
-                                   UI.hr # set style [("width","75%")]
-                                   ]
-                                   ,
+                                           [element wr_input, element wor_input]
+                                   ] # set style [
+                                          ("padding-left", "47px"), ("padding-top", "7px")
+                                          ],
+                                   grid [
+                                           [element text_field_k , element field_k],
+                                           [element text_field_m , element field_m]
+                                        ] # set style [
+                                          ("text-align", "center"),
+                                          ("padding-top", "22px"),
+                                          ("padding-left", "35px"),
+                                          ("padding-bottom", "5px")
+                                          ]
+                                   ],
                             column [
                                    grid [
-                                           [string "\\( |D| :\\)", element field_domain],
-                                           [string "\\( | S \\cap R | :\\)", element field_card1],
-                                           [string "\\( | S \\setminus R | :\\)", element field_card2],
-                                           [string "\\( | R \\setminus S | :\\)", element field_card3]
-                                        ],
-                                   UI.hr
+                                           [element text_field_domain, element field_domain],
+                                           [element text_field_card1, element field_card1],
+                                           [element text_field_card2, element field_card2],
+                                           [element text_field_card3, element field_card3]
+                                        ]  # set style [
+                                          ("text-align", "center"),
+                                          ("padding-left", "35px"),
+                                          ("padding-bottom", "5px"),
+                                          ("padding-top", "5px")
+                                          ]
                                    ]
-                            ],
-                        row [element button, element field_samplesize],
-                        row [UI.span # set text "Formula approximately evaluates to: ", element resultApp],
-                        row [UI.span # set text "Formula evaluates to: ", element result ]
-                       ]
-                   # set style [
-                     ("background","lightskyblue"),
-                     ("text-align", "left"),
-                     ("color","black"),
-                     ("font-size","20px"),
-                     ("width","100%")
+                            ]# set style [
+                              ("background","rgba(11, 127, 171,0.4)"),
+                              ("padding-bottom", "5px")
+                              ] ,
+                            UI.hr # set style [("border-top", "1px solid darkblue")],
+                            row [element text_field_samplesize, element field_samplesize] # set style [
+                                  ("padding-top", "5px"),
+                                  ("padding-left", "45px")
+                                ],
+                            row [element button] # set style [
+                                  ("padding-left", "137px"),
+                                  ("padding-top", "15px")
+                                ],
+                            row [UI.span # set text "Formula approximately evaluates to: ", element resultApp] # set style [
+                                  ("text-align", "right"), ("padding-top", "15px"), ("color", "blue")],
+                            row [UI.span # set text "Formula evaluates to: ", element result ] # set style [
+                                  ("text-align", "right"), ("padding-top", "5px"), ("color", "blue")]
+                       ] # set style [
+                           ("background-color","lightskyblue"),
+                           ("text-align", "right"),
+                           ("color","black"),
+                           ("font-size","10px"),
+                           ("height", "200px"),
+                           ("width","370px"),
+                           ("border", "2px solid blue"),
+                           ("padding-bottom", "61px")
                      ]
 
     let startGameFunction = do
